5db8f89f202424e754711a7b93f5e1b0bbfd40c1
hg00521 <hg00521@xinics.com>
2020-03-04 16:27:20 +0900

    mongodb connect reactive practice

--- reactivedb/src/main/java/org/raccoon/reactivedb/CustomRepository.kt
+++ reactivedb/src/main/java/org/raccoon/reactivedb/CustomRepository.kt
@@ -1,9 +1,16 @@
 package org.raccoon.reactivedb
 
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.findById
+import org.springframework.data.mongodb.core.query.Criteria.where
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.isEqualTo
+import org.springframework.data.mongodb.core.remove
+import org.springframework.data.mongodb.core.find
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
+import sun.jvm.hotspot.tools.jcore.NameFilter
 import javax.annotation.PostConstruct
 
 @Repository
@@ -18,6 +25,11 @@ class CustomRepository(private val template: ReactiveMongoTemplate){
     fun initializeRepository() = initialCustomers.map(Customer::toMono).map(this::create).map(Mono<Customer>::subscribe)
 
     fun create(customer: Mono<Customer>) = template.save(customer)
+    fun findById(id:Int)=template.findById<Customer>(id)
+    fun deleteById(id:Int)=template.remove<Customer>(Query(where("_id").isEqualTo(id)))
+    fun findCustomer(nameFilter: NameFilter)=template.find<Customer>(
+            Query(where("name").regex(".*$nameFilter.*","i"))
+    )
 }
 //모노나 플럭스같은 리액티브 타입으로 작업을 수행하기 위해서 바꿈
 
---
+++ reactivedb/src/main/java/org/raccoon/reactivedb/CustomerServiceImpl.kt
@@ -0,0 +1,20 @@
+package org.raccoon.reactivedb
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@Service
+class CustomerServiceImpl : CustomerService {
+    @Autowired
+    lateinit var customerRepository: CustomRepository
+
+    override fun getCustomer(id: Int): Mono<Customer> =customerRepository.findById(id)
+
+    override fun createCustomer(customer: Mono<Customer>): Mono<Customer> = customerRepository.create(customer)
+    override fun deleteCustomer(id: Int): Mono<Boolean> = customerRepository.deleteById(id).map{it.deletedCount>0}
+    override fun searchCustomers(nameFilter: String): Flux<Customer> =customerRepository.findCustomer(nameFilter)
+
+
+}
